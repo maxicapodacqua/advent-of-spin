d73b4ba6663e0dd84927f94b451888ccdbf7b6d4
Maxi Capodacqua <kapomaxi1@gmail.com>
2023-11-30 19:16:24 -0600

    Reading from store

--- challenge-1/backend/src/index.ts
+++ challenge-1/backend/src/index.ts
@@ -1,31 +1,89 @@
-import { HandleRequest, HttpRequest, HttpResponse } from "@fermyon/spin-sdk";
+import {
+  HandleRequest,
+  HttpRequest,
+  HttpResponse,
+  Kv,
+  Router,
+} from "@fermyon/spin-sdk";
 
-async function GET(request: HttpRequest): Promise<HttpResponse> {
+type KvStoreType = ReturnType<(typeof Kv)["openDefault"]>;
+
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
+
+const router = Router();
+
+router.get("/data", (req) => {
+  if (!req.query) {
+    return {
+      status: 400,
+      headers: { "content-type": "application/json" },
+      body: "Missing wishlist key :(",
+    };
+  }
+  const key = Object.keys(req.query)[0];
+  const store = Kv.openDefault();
+  console.log("key", key);
+
+  const wishlist = store.get(key);
+
   return {
     status: 200,
     headers: { "content-type": "application/json" },
-    body: "GET call",
+    body: JSON.stringify({
+      value: wishlist,
+    }),
   };
-}
-async function POST(request: HttpRequest): Promise<HttpResponse> {
+});
+
+router.post("/data", (req) => {
   return {
-    status: 201,
+    status: 200,
     headers: { "content-type": "application/json" },
-    body: "POST call ..",
+    body: "POST call",
   };
-}
+});
+
+router.all("*", () => ({
+  status: 404,
+  body: encoder.encode("Not found"),
+}));
+
+// async function GET(
+//   request: HttpRequest,
+//   store: KvStoreType
+// ): Promise<HttpResponse> {
+//   // store.get()
+//   return {
+//     status: 200,
+//     headers: { "content-type": "application/json" },
+//     body: "GET call",
+//   };
+// }
+// async function POST(
+//   request: HttpRequest,
+//   store: KvStoreType
+// ): Promise<HttpResponse> {
+//   return {
+//     status: 201,
+//     headers: { "content-type": "application/json" },
+//     body: "POST call ..",
+//   };
+// }
 export const handleRequest: HandleRequest = async function (
   request: HttpRequest
 ): Promise<HttpResponse> {
-  switch (request.method) {
-    case "GET":
-      return await GET(request);
-    case "POST":
-      return await POST(request);
-  }
-  return {
-    status: 405,
-    headers: { "content-type": "application/json" },
-    body: `Invalid ${request.method} method call`,
-  };
+  return await router.handleRequest(request);
+  // const store = Kv.openDefault();
+  // switch (request.method) {
+  //   case "GET":
+  //     return await GET(request, store);
+  //   case "POST":
+  //     return await POST(request, store);
+  // }
+  // return {
+  //   status: 405,
+  //   headers: { "content-type": "application/json" },
+  //   body: `Invalid ${request.method} method call`,
+  // };
 };
