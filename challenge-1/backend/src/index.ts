a6d7955c38f3061058acde552ff1161c23eac318
Maxi Capodacqua <kapomaxi1@gmail.com>
2023-11-30 18:47:01 -0600

    Moving into functions

--- challenge-1/backend/src/index.ts
+++ challenge-1/backend/src/index.ts
@@ -1,9 +1,31 @@
-import { HandleRequest, HttpRequest, HttpResponse } from "@fermyon/spin-sdk"
+import { HandleRequest, HttpRequest, HttpResponse } from "@fermyon/spin-sdk";
 
-export const handleRequest: HandleRequest = async function (request: HttpRequest): Promise<HttpResponse> {
+async function GET(request: HttpRequest): Promise<HttpResponse> {
   return {
     status: 200,
-    headers: { "content-type": "text/plain" },
-    body: "Hello from TS-SDK"
-  }
+    headers: { "content-type": "application/json" },
+    body: "GET call",
+  };
 }
+async function POST(request: HttpRequest): Promise<HttpResponse> {
+  return {
+    status: 201,
+    headers: { "content-type": "application/json" },
+    body: "POST call ..",
+  };
+}
+export const handleRequest: HandleRequest = async function (
+  request: HttpRequest
+): Promise<HttpResponse> {
+  switch (request.method) {
+    case "GET":
+      return await GET(request);
+    case "POST":
+      return await POST(request);
+  }
+  return {
+    status: 405,
+    headers: { "content-type": "application/json" },
+    body: `Invalid ${request.method} method call`,
+  };
+};
